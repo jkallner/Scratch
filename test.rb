cfda9add74c101dc0e089e12c3b064f4f787d7d1
Jason Kallner <jkallner@gmail.com>
2013-08-20 22:17:15 -0400

    with hashes

--- test.rb
+++ test.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 def circle_info(radius)
-  return [Math::PI * (radius * radius), Math::PI * (2 * radius)]
+  {:area => Math::PI * (radius * radius), :perimeter => Math::PI * (2 * radius)}
 end
-area, perimeter = circle_info(10)
-  puts "The area of the circle is: #{area}"
-  puts "The perimeter of the circle is: #{perimeter}"
+results_hash = circle_info(10)
+  puts "The area of the circle is: #{results_hash[:area]}"
+  puts "The perimeter of the circle is: #{results_hash[:perimeter]}"
   
 
 
