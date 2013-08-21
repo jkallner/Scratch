ea2cae29339e6cf17b47703401c65c0888dccb86
Jason Kallner <jkallner@gmail.com>
2013-08-21 16:32:06 -0400

    Input and variables
    Changed program to prompt for input and convert response to float
    instead of string.

--- test.rb
+++ test.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
+puts "Please input the radius of the circle:"
+input_radius = gets.to_f
 def circle_info(radius)
   {:area => Math::PI * (radius * radius), :perimeter => Math::PI * (2 * radius)}
 end
-results_hash = circle_info(10)
+results_hash = circle_info(input_radius)
   puts "The area of the circle is: #{results_hash[:area]}"
   puts "The perimeter of the circle is: #{results_hash[:perimeter]}"
   
