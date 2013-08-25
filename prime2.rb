8af1174d1fb7f9b9a606ba0792d70cc01c648b41
Jason Kallner <jkallner@gmail.com>
2013-08-25 00:16:24 -0400

    latest ver

--- prime2.rb
+++ prime2.rb
@@ -1,34 +1,34 @@
 #!/usr/bin/env ruby
-#now the class that will determine whether or not a number is prime
-#class PrimeNum
-#  def initialize(input_number)
-#    @input_number = input_number
-#  end
  
- 
- puts "Pleas input a number to test if its a prime number: "
- input_number = gets.to_f
- 
+#Ask the user for a number to test if it is a prime number or not 
+puts "Pleas input a number to test if its a prime number: "
+input_number = gets.to_f
+
+#Ensure that the user doesn't put in zero as zero isn't a prime (division by zero)
+#Also note that by some definitions negative numbers are in fact prime numbers and therefore valid
+while input_number == 0
+puts "You submitted and invalid number or zero, please input a different number: "
+input_number = gets.to_f
+end
+
 #Method for testing if a given number is prime or not
 def is_prime(input_number)
   divisible_counter = 0
-# (1..input_number).each do |x|
-  for x in 1..input_number do
-      if input_number % x == 0
+   for x in 1..input_number do
+    if input_number % x == 0
       divisible_counter += 1
-      puts divisible_counter
       break if divisible_counter > 2
-      end
-  end
-  if divisible_counter < 3
-    test_results = true
-  else
-    test_results = false
-  end
-  return test_results
+    end
+   end
+    if divisible_counter < 3
+      test_results = true
+    else
+      test_results = false
+    end
+    return test_results
 end
 
-
+#Show the results of our test
 results = is_prime(input_number)
-puts results
+puts "\n#{results}"
 
