fb2b408f3171fa68b86ab0dabc07b779df375f98
Jason Kallner <jkallner@gmail.com>
2013-08-25 16:43:39 -0400

    prime.rb application updated
    This update has a new file for prime.rb.  It takes the method from
    primer2.rb that tests whether or not a number is prime and expands upon
    it by taking two input numbers and displaying all prime numbers between
    them.

--- prime.rb
+++ prime.rb
@@ -1,35 +1,47 @@
 #!/usr/bin/env ruby
+
+#Initial display the tell the user what the program does and then asks for input
 puts "This program will find all the prime numbers between the two numbers you input."
 puts "Please enter two numbers to search between other than 0 of course :)"
 puts "Enter number 1:"
-number_1 = gets.to_f
+number_1 = gets.to_i
 puts "Enter number 2:"
-number_2 = gets.to_f
+number_2 = gets.to_i
 
 #This loop ensures that the number entered is not 0
 while number_1 == 0 or number_2 == 0
   puts "You put a number in that was invalid or equalled zero. try again."
   puts "Enter number 1:"
-  number_1 = gets.to_f
+  number_1 = gets.to_i
   puts "Enter number 2:"
-  number_2 = gets.to_f
+  number_2 = gets.to_i
 end
 
-#now the class that will determine whether or not a number is prime
-class PrimeNum
-  def initialize(input_number)
-    @input_number = input_number
+#Method for testing if a given number is prime or not
+def is_prime(input_number)
+  divisible_counter = 0
+  (1..input_number).each do |x|
+   if input_number % x == 0
+     divisible_counter += 1
+     return false if divisible_counter > 2
+   end
   end
-  
-  #Method for testing if a given number is prime or not
-  def is_prime?(@input_number)
-    i = 1
-    while i <= @input_number
-      if @input_number % i == 0 && divisible_counter < 3
-        divisible_counter.next
-        i.next
-      else
-      break return false
-      end
-    return true
+  true
+end
+
+#Here we will cycle through all the numbers between number_1 and number_2 and test each one to determine
+#if it is a prime number or not.  If it is, it gets appended to the array
+prime_numbers = Array.new
+(number_1..number_2).each do |x|
+  if is_prime(x)
+    prime_numbers << x
   end
+end
+
+#tests to see if 0 was included in the list and then removes it if so
+if prime_numbers.include? 0
+  prime_numbers.delete(0)
+end
+
+#Display our results
+puts "The prime numbers between the two provided numbers are:\n #{prime_numbers}"
