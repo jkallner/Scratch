5bd24b9c8e2f5d5b133bc3d94554af14623791e0
Jason Kallner <jkallner@gmail.com>
2013-09-12 13:08:01 -0400

    20130912-1
    experimented with reverse order and top 5

--- word_count.rb
+++ word_count.rb
@@ -10,8 +10,8 @@ raw_text = %{
   
 word_list = words_from_string(raw_text)
 counts = count_frequency(word_list)
-sorted = counts.sort_by {|word, count| count}
-top_five = sorted.last(5)
+sorted = counts.sort_by {|word, count| count}.reverse
+top_five = sorted.first(5)
 
 top_five.each do |word, count|
   puts "#{word}:  #{count}"
